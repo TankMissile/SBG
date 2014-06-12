154c0cecc4ffaa228e3090f8bb8866a746b8f979
VoidGazer <voidgazerwebsite@gmail.com>
2014-06-12 15:36:35 -0500

    Seeing how this works

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -2,25 +2,27 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
-public class GameMenu extends JPanel implements ActionListener{
+public class GameMenu extends JPanel implements ActionListener
+{
 	private static final long serialVersionUID = 1L;
 	
 	private JButton resume_btn;
 	private JButton exit_btn;
 	private JButton level_editor_btn;
 	
-
-
-	public GameMenu(){
-		//this.setSize(new Dimension(300,500));
-		//this.setla
-		this.setBounds(ClientWindow.WIDTH/2-150, ClientWindow.HEIGHT/2-200, 300, 500);
-		
+	
+	
+	public GameMenu()
+	{
+		this.setBounds(ClientWindow.WIDTH/2-150, ClientWindow.HEIGHT/2-200, 300, 500);		
+		this.setBorder(BorderFactory.createRaisedSoftBevelBorder());
 		this.setBackground(Color.WHITE);
+
 		exit_btn = new JButton("Exit");
 		resume_btn = new JButton("Resume");
 		level_editor_btn = new JButton("Level Editor");
@@ -32,21 +34,39 @@ public class GameMenu extends JPanel implements ActionListener{
 		this.setVisible(false);
 	}
 	
-	public void open(boolean b){
-		if(b)
-		{
-			this.setVisible(true);
-		}
-		else
-		{
-			this.setVisible(false);
-		}
+	
+	
+	public void open(boolean b)
+	{
+		if(b) this.setVisible(true);
+		else  this.setVisible(false);
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e)
+	{
 		if(e.getSource().getClass().equals("JButton"));
-		
+		{
+			JButton button_clicked = (JButton) e.getSource();
+			
+			if(button_clicked.getText().compareTo("Exit") == 0)
+			{
+				/* TODO: What kind of cleanup needs to be done before exiting? */
+				System.out.println("Exit functionality coming soon");
+				System.exit(1);
+			}
+			else if(button_clicked.getText().compareTo("Resume") == 0)
+			{
+				/* TODO: How does this menu pause the game? We need to un-pause at this point */
+				System.out.println("Resume functionality coming soon");
+				this.open(false);
+			}
+			else if(button_clicked.getText().compareTo("Level Editor") == 0)
+			{
+				/* TODO: Why exactly is this a menu option? Do we want to be able to edit the current level? */
+				System.out.println("Level Editor functionality coming soon");
+			}
+		}
 		
 	}
 }
