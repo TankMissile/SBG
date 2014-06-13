95894c403df566d0af25d81597a970788004dc8c
Adam Perez <aperez60@uic.edu>
2014-06-12 21:15:20 -0500

    Made in-game menu work, basically

--- src/GameMenu.java
+++ src/GameMenu.java
@@ -1,72 +1,72 @@
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 
 public class GameMenu extends JPanel implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
-	
+
 	private JButton resume_btn;
 	private JButton exit_btn;
 	private JButton level_editor_btn;
-	
-	
-	
+
+
+
 	public GameMenu()
 	{
-		this.setBounds(ClientWindow.WIDTH/2-150, ClientWindow.HEIGHT/2-200, 300, 500);		
+		this.setBounds(ClientWindow.WIDTH/2-150, ClientWindow.HEIGHT/2-200, 300, 500);
+		this.setLayout(new GridLayout(3, 1));
 		this.setBorder(BorderFactory.createRaisedSoftBevelBorder());
 		this.setBackground(Color.WHITE);
 
 		exit_btn = new JButton("Exit");
 		resume_btn = new JButton("Resume");
 		level_editor_btn = new JButton("Level Editor");
-		
+
+		exit_btn.addActionListener(this);
+		resume_btn.addActionListener(this);
+		level_editor_btn.addActionListener(this);
+
 		this.add(resume_btn);
 		this.add(exit_btn);
 		this.add(level_editor_btn);
-		
+
 		this.setVisible(false);
 	}
-	
-	
-	
-	public void open(boolean b)
-	{
-		if(b) this.setVisible(true);
-		else  this.setVisible(false);
-	}
-	
+
+
+
+	public void open(boolean b) { this.setVisible(b); }
+
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		if(e.getSource().getClass().equals("JButton"));
+		JButton button_clicked = (JButton) e.getSource();
+
+		if(button_clicked == exit_btn)
 		{
-			JButton button_clicked = (JButton) e.getSource();
-			
-			if(button_clicked.getText().compareTo("Exit") == 0)
-			{
-				/* TODO: What kind of cleanup needs to be done before exiting? */
-				System.out.println("Exit functionality coming soon");
-				System.exit(1);
-			}
-			else if(button_clicked.getText().compareTo("Resume") == 0)
-			{
-				/* TODO: How does this menu pause the game? We need to un-pause at this point */
-				System.out.println("Resume functionality coming soon");
-				this.open(false);
-			}
-			else if(button_clicked.getText().compareTo("Level Editor") == 0)
-			{
-				/* TODO: Why exactly is this a menu option? Do we want to be able to edit the current level? */
-				System.out.println("Level Editor functionality coming soon");
-			}
+			/* TODO: What kind of cleanup needs to be done before exiting? */
+			System.exit(1);
 		}
 		
+		if(button_clicked == resume_btn)
+		{
+			/* TODO: This takes a while... figure out why */			
+			ClientWindow cw = (ClientWindow) SwingUtilities.getAncestorOfClass(ClientWindow.class, this);
+			cw.level.openGameMenu(false);
+		}
+		
+		if(button_clicked == level_editor_btn)
+		{
+			/* TODO: Why exactly is this a menu option? Do we want to be able to edit the current level? */
+			System.out.println("Level Editor functionality coming soon");
+		}		
 	}
 }
